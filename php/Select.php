7366cfa13774f0b8988ce41909e1d39d6eab961d
vladimirgor <vladimirgor2011@yandex.ru>
2017-04-17 16:39:44 +0500

    Select.php added

--- php/Select.php
+++ php/Select.php
@@ -1,7 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Владимир
- * Date: 17.04.2017
- * Time: 15:44
- */
+
+include_once('config.php');
+require_once('functions.php');
+/*
+if ($session != session_id()) {
+    $message[] = ['message' => "Возможно предпринята XSS атака."];
+    echo json_encode($message);
+    die;
+}
+*/
+$link = dbLink();
+$sql = "SELECT `name`,`comment`,`email` FROM " . TABLE . " ORDER BY `id` DESC";
+$comment_list = Select($sql, $link);
+echo json_encode($comment_list);
+
