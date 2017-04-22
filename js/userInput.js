caedde0c41e01387454611012c181b051d528869
vladimirgor <vladimirgor2011@yandex.ru>
2017-04-23 00:35:07 +0500

    class changed to ticket for whole card and str1, str2, str3 for card lines

--- js/userInput.js
+++ js/userInput.js
@@ -39,7 +39,6 @@ $(document).ready(function(){
 });
 /*  ajax response processing  */
 function processResultSelect(json)	{
-    var str = "";
     var offset = "";
 /* cards html cleaning */
     $("#list").html("");
@@ -55,13 +54,11 @@ function processResultSelect(json)	{
 /* cards  html forming */
             if ( i % 3 == 0 ) offset = " col-sm-offset-1 ";
             else offset = "";
-            if (i % 2 == 0) str = 'c'; /* even card class */
-            else str = 'nc';           /* odd  card class */
             $("#list").append(
-                '<div class="col-sm-3 ticket' + str + offset + '">' +
-                '<p class="' + str + '1' + '">' + e.name + '</p>' +
-                '<p class="' + str + '2' + '">' + e.email + '</p>' +
-                '<p class="' + str + '3' + '">' + e.comment + '</p>' +
+                '<div class="col-sm-3 ticket' + offset + '">' +
+                '<p class="str1">' + e.name + '</p>' +
+                '<p class="str2">' + e.email + '</p>' +
+                '<p class="str3">' + e.comment + '</p>' +
                 '</div>');
         }
     });
